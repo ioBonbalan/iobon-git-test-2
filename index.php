39d2d7d1e23bf17693c735c30375f9977f7eae47
Io Bonbalan <adnanbonbalan@gmail.com>
2024-04-23 00:32:30 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-    <h1>Hello World Rise n Fucking Shine</h1
+    <h1>Hello World Rise n Fucking Shine</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, voluptas fuga, facere nihil dicta ab est odio nesciunt neque amet, nisi blanditiis! Iusto aliquam maiores eaque cumque natus soluta provident.</p>
 </body>
 </html>
